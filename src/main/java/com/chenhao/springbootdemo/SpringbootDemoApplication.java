e500a04a3c3b823f76c5387503addf2d82ea4450
TcMysunshine <2229238008@qq.com>
2018-12-23 17:45:22 +0800

    发布到tomcat需要去掉内嵌服务器

--- src/main/java/com/chenhao/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/chenhao/springbootdemo/SpringbootDemoApplication.java
@@ -2,10 +2,16 @@ package com.chenhao.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootDemoApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
